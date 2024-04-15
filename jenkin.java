1febdd764a20480353200cc50e15afc6d59cfbd4
manan3101 <114339274+manan3101@users.noreply.github.com>
2024-04-15 18:16:41 +0530

    Add files via upload

---
+++ jenkin.java
@@ -0,0 +1,6 @@
+public class jenkin {
+    public static void main(String[] args) {
+        System.out.println("this is LAB_FAT jenkin's implementation");
+         System.out.println("MANAN AGGARWAL 20MIC0087");
+    }
+}
